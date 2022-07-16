714199324fffa7fa0c7b801fa9f308ff551e55da
chizi56 <bauka1215@gmail.com>
2022-07-16 18:53:43 +0600

    fix: env file

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const Provider = require('@truffle/hdwallet-provider');
 const abi = require('./utils/Bet.json');
 require("dotenv").config();
 
-const uri = "mongodb+srv://baurma-admin:NKM9DVeH5!LiFyY@mongo.miikc.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.URI;
 const client = new MongoClient(uri);
 
 var app = express();
 app.use(cors());
 app.use(express.json());
+
 var port = process.env.PORT || 8080;
 
 var SmartContractAddress = "0x5CBc5735309FB70767f3820d9E561F1b74133473";
@@ -28,8 +29,9 @@ client
   .catch((err) => {
     console.log(err);
   });
-app.get("/join/:roomId", async (req, res) => {
+app.get("/api/join/:roomId", async (req, res) => {
   const {roomId} = req.params;
+  
   const db = client.db('test');
   const games = db.collection('games');
   console.log("Find game room", roomId);
@@ -38,30 +40,30 @@ app.get("/join/:roomId", async (req, res) => {
   res.send(game);
 }) 
 
-app.get("/games", async (req, res) => {
+app.get("/api/games", async (req, res) => {
   const db = client.db("test");
   const collection = db.collection("games");
-
   const result = await collection.find({}).toArray();
-
   res.send(result);
 })
-app.post("/create-game", async (req, res) => {
+
+app.post("/api/create-game", async (req, res) => {
     const { signerAddress,nickname,secondNickname,amount} = req.body;
+    const over = false;
     const collection = client.db().collection("games");
     const response = await collection.insertOne({
         signerAddress,
         nickname,
         secondNickname,
-        // link,
-        amount
+        amount, 
+        over
     })
     const result = {
         id: response.insertedId
     }
     res.send(result);
 })
-app.post("/lobby/:roomId", async (req,res) => {
+app.post("/api/lobby/:roomId", async (req,res) => {
   const { secondSigner } = req.body;
   const {roomId } = req.params;
   const collection = client.db().collection("games");
@@ -92,11 +94,30 @@ const sendData = async ({winner, bet}) => {
   }
 }
 
-app.post("/withdraw", (req, res) => {
+app.post("/api/withdraw/:roomId",async (req, res) => {
   const { winner, amount } = req.body;
-  const bet = amount - (amount * 0.1);
-  console.log(winner, bet);
-  sendData({winner, bet});
+  console.log(winner);
+  const db = client.db("test");
+  const collection = db.collection("games");
+  const {roomId} = req.params;
+  console.log("Find game room", roomId);
+  const game = await collection.findOne({_id: ObjectId(roomId)});
+  console.log(game);
+  if(game.over === true){
+    res.send(`The money has been already sent to ${winner.substr(0, 7)} address` );
+    console.log("The money has been already sent to the winner!");
+  }
+  else{
+    
+    const bet = amount - (amount * 0.1);
+    const updated = await collection.updateOne({_id: ObjectId(roomId)}, {
+      $set: {over: true}
+    })
+    console.log(winner, bet);
+    await sendData({winner, bet});
+    res.send("Cash was delivered!")
+  }
+  
 })
 
 app.listen(port);
